fe77018086ccbdfb7d0626287fefe866430e8e6b
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2019-12-02 15:33:29 -0500

    Dec2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
     // document.getElementById("bbody").style.cursor = "pointer";
     // document.getElementById("bbody").css( 'cursor', 'url(smile.png), auto' );
-    $('body').css('cursor', 'url(smile.png), auto');
+    $('body').css('cursor', 'url(https://sssssuty.github.io/websiteDec2019/smile.png), auto');
 });
 
 

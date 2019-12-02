4dad74c0ca570c950fb8c5bc2410ce0929500e9d
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2019-12-02 17:57:32 -0500

    Dec2

--- indexmain.js
+++ indexmain.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
     // document.getElementById("bbody").style.cursor = "pointer";
     // document.getElementById("bbody").css( 'cursor', 'url(smile.png), auto' );
-    $('body').css('cursor', 'url(http://sssssuty.github.io/websiteDec2019/smile.png), auto');
+    // $('body').css('cursor', 'url(https://www.sutingyu.com/Projects/Displacement/cover.png), auto');
+    console.log( "ready!" );
 });
 
 
